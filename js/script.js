1918f5e840b74a70dcad7099e7d2b0f789ff357f
sean <seancaetanomartin@gmail.com>
2011-09-13 00:41:29 -0700

    created new branch to play with experimental ideas

--- js/script.js
+++ js/script.js
@@ -1,66 +1,17 @@
 $(function () {
-   
-   // xonecas namespace
-   // -----------------
-   var x = window.x = {};
-
-   // Helpers
-   // -------
-   // {{{ 8< 
-   function relativeTime(time) {
-      time = _.isNumber(time) ? time * 1000 : time;
-
-      var then = +new Date(time),
-         now = +new Date(),
-         timestamp = now - then,
-         days = 0, hours = 0;
-      
-      days = Math.floor(timestamp / (1000 * 60 * 60 * 24));
-      hours = Math.floor(timestamp / (1000 * 60 * 60));
-
-      if (days === 1)
-         timestamp = "yesterday";
-      else if (days > 31) 
-         timestamp = "more than a month ago";
-      else if (days > 1) 
-         timestamp = days + " days ago";
-      else if (hours === 1)
-         timestamp = "an hour ago";
-      else if (hours > 1)
-         timestamp = hours + " hours ago";
-      else 
-         timestamp = "a few moments ago";
-
-      return timestamp +".";
-   }
-
-   function linkit (text) {
-      text = text.replace(
-         /(ftp|http|https|file):\/\/[\S]+(\b|$)/gim, 
-         '<a href="$&" target="_new">$&</a>');
-      text = text.replace(/@\w+\s/gi, function (match) {
-         match = match.substring(0, match.length -1);
-         return '<a href="//twitter.com/'+match.substring(1)+'">'+match+'</a> ';
-      });
-
-      return text;
-   }
-   // }}}
-
-   // Tumblr client
-   // -------------
-   // {{{ 8< 
-   x.Blog = Backbone.Collection.extend({
+   Tumblr = Backbone.Collection.extend({
       url: "http://api.tumblr.com/v2/blog/xonecas.tumblr.com/posts/text",
       page: 0,
-      count: 5,
+      count: 6,
+      idx: 0,
+
+      initialize: function () {
+         _.bindAll(this, "setCurrent");
+         this.bind('reset', this.setCurrent);
+      },
 
       parse: function (res, xhr) {
-
-         _.each(res.response.posts, function (post, idx) {
-            post.relTime = relativeTime(post.timestamp);
-         });
-
+         this.total = res.response.total_posts;
          return res.response.posts;
       },
 
@@ -77,173 +28,91 @@ $(function () {
          });
 
          return $.ajax(params);
-      }
-   });
-   
-   x.BlogView = Backbone.View.extend({
-      collection: new x.Blog(),
-      el: $('#tumblr'),
-      template: $('#_tumblr').html(),
-
-      initialize: function () {
-         this.collection.fetch();
-
-         _.bindAll(this, 'render');
-         this.collection.bind('reset', this.render);
       },
 
-      render: function () {
-         var that = this;
-
-         this.el.empty();
-         this.collection.forEach(function (el) {
-            that.el.append(
-               _.template(that.template, el.toJSON())
-            );
-         });
-         this.visible = 0;
-         $("#"+this.collection.at(0).get('id'))
-            .addClass('selectedPost');
-      }
-   });
-   // }}}
-
-   // Twitter client
-   // --------------
-   // {{{ 8< 
-   x.Tweets = Backbone.Collection.extend({
-      url: "http://api.twitter.com/1/statuses/user_timeline.json",
-
-      parse: function (res, xhr) {
-         _.each(res, function (tweet, idx) {
-            tweet.relTime = relativeTime(tweet.created_at);
-            tweet.text = linkit(tweet.text);
-         });
-
-         return res;
+      setCurrent: function () {
+         this.current = this.at(this.idx);
       },
 
-      sync: function (model, method, options) {
-         var params = _.extend(options, {
-            "url": this.url,
-            "data": {
-               "screen_name": "xonecas",
-               "count": 10,
-               "trim_user": true,
-               "include_rts": true,
-               "exclude_replies": true
-            },
-            "dataType": "jsonp"
-         });
+      next: function () {
+         this.idx++;
+         var model = this.at(this.idx);
 
-         return $.ajax(params);
-      }
-
-   });
-
-   x.TweetsView = Backbone.View.extend({
-      collection: new x.Tweets(),
-      el: $('#latestTweet'),
-      template: $('#_latestTweet').html(),
-
-      initialize: function () {
-         _.bindAll(this, 'render', 'loop');
-         this.collection.bind('reset', this.loop);
-         this.collection.fetch();
+         if (model) {
+            this.current = model;
+         } else {
+            this.page++;
+            this.idx = 0;
+            this.fetch();
+         }
       },
 
-      render: function () {
-         var random = Math.floor(Math.random() * this.collection.length),
-            tweet = this.collection.at(random);
+      previous: function () {
+         this.idx--;
+         var model = this.at(this.idx);
 
-         this.el.html(_.template(this.template, tweet.toJSON()));
-      },
-
-      loop: function () {
-         this.render();
-
-         setTimeout(this.loop, 60 * 1000);
-      }
-   });
-   // }}}
-
-   // Coderwall client
-   // ----------------
-   // {{{ 8< 
-   x.Badges = Backbone.Collection.extend({
-      url: "http://coderwall.com/xonecas.json",
-
-      parse: function (res, xhr) {
-         return res.data.badges;
-      },
-   
-      sync: function (model, method, options) {
-         var params = _.extend(options, {
-            "url": this.url,
-            "dataType": "jsonp"
-         });
-
-         return $.ajax(params);
-      }
-   });
-
-   x.BadgesView = Backbone.View.extend({
-      collection: new x.Badges(),
-      el: $("#coderwall"),
-      template: $('#_coderwall').html(),
-
-      initialize: function () {
-         _.bindAll(this, 'render');
-         this.collection.bind('reset', this.render);
-         this.collection.fetch();
-      },
-
-      render: function () {
-         var that = this;
-
-         this.el.empty();
-         this.collection.each(function (badge) {
-            that.el.append(_.template(that.template, badge.toJSON()));
-         });
-      }
-   });
-   // }}}
-
-   x.IndexView = Backbone.View.extend({
-      el: $(document),
-
-      initialize: function () {
-         this.blog = new x.BlogView();
-         this.tweet = new x.TweetsView();
-         this.badges = new x.BadgesView();
-      },
-
-      events: {
-         'click' : "keypressRouter"
-      },
-
-      keypressRouter: function (ev) {
-         switch (ev.which) {
-         case 106:
-         case 107:
-            this.jumpTo(ev);
-         break;
-         case 47:
-            ev.preventDefault();
-         break;
-         default:
-            if (console && console.log) {
-               console.log(ev.which);
+         if (model) {
+            this.current = model;
+         } else {
+            if (this.page > 0) {
+               this.page--;
+               this.idx = this.count -1;
+               this.fetch();
+            } else {
+               this.trigger('error', 'At most recent post');
             }
          }
 
       }
    });
 
-   // main
-   // ----
-   x.index = new x.IndexView();
-   
-   return x;
+   ErrorReport = Backbone.View.extend({
+      id:         'error',
+      className:  'container',
+      template:   Handlebars.compile($('#error_tmpl').html()),
 
+      initialize: function () {
+         _.bindAll(this, "remove");
+         this.render();
+      },
+
+      render: function () {
+         $('#main').append(this.template(this.error));
+         setTimeout(this.remove, 5000);
+         return this;
+      }
+   });
+
+   HomePage = Backbone.View.extend({
+      id:         'main',
+      className:  'container',
+      template:   Handlebars.compile($('#post_tmpl').html()),
+      collection: new Tumblr(),
+
+      initialize: function () {
+         _.bindAll(this, "render", "report");
+         this.collection.bind('reset', this.render);
+         this.collection.bind('error', this.report);
+         this.collection.fetch();
+      
+         $(this.el).appendTo('body');
+      },
+
+      render: function () {
+         var model = this.collection.current;
+         $(this.el)
+            .css('display', 'none')
+            .html(this.template(model.toJSON()))
+            .fadeIn(1000);
+         
+      },
+
+      report: function (error) {
+         this.errors = this.errors || [];
+         this.errors.push(new ErrorReport().render());
+      }
+   });
+
+   // main
+   window.app = new HomePage();
 });
