9f443e943b382ca7b814ab112fa3d5d870f35cbc
Sean Caetano Martin <seancaetanomartin@gmail.com>
2011-10-27 02:17:44 -0700

    requirejs, and compiling less serverside.

---
+++ js/init.js
@@ -0,0 +1,28 @@
+require.config({
+   baseUrl: '/',
+   paths: {
+      bootstrap: 'bootstrap/js/'
+   }
+});
+
+// get all of the dependencies
+require([
+   'vendor/json2',
+   'vendor/jquery',
+   'vendor/handlebars',
+   'vendor/underscore',
+   'vendor/hijs',
+   'vendor/smoke',
+], function () {
+   // these need jquery
+   require([
+      'vendor/backbone',
+      'bootstrap/bootstrap-dropdown'
+   ], function () {
+      // init the app
+      require(['js/script.js'], function () {
+         console && console.log && console.log("OK");
+      });
+
+   });
+});
