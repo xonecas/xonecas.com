0475ed00adb2f930459f2de31adff6c44ff989dc
xonecas <xonecas@edshelf.com>
2012-06-10 19:29:05 -0700

    change in the colors, canvas only for ie9 and others

--- http.js
+++ http.js
@@ -7,7 +7,7 @@ server = connect()
     .use(connect.favicon('favicon.ico'))
     .use(connect.logger('dev'))
     .use(connect.compress())
-    .use(connect['static'](root))
+    .use(connect['static'](root, { maxAge: 31536000000 })) // one year.
     .use(connect.errorHandler())
     .listen(process.argv[2] ? 80 : 8080);
 
